d3b6cf6b257fdf482c36b4ed82f2e684061fb2be
Cony Jerez <constanzajerez89@gmail.com>
2023-04-30 19:04:31 -0400

    Update

--- JS/JS.js
+++ JS/JS.js
@@ -1,8 +1,23 @@
 const modal = document.querySelector('.modal');
 const buttonSpider = document.querySelector('#button-spider');
-const closeButton = document.querySelector('.modal-content-close');
+const buttonRobin = document.querySelector('#button-robin');
+const buttonBatman = document.querySelector('#button-batman');
+const closeButton = document.querySelector('.modal-content-close');/*img*/
+
 
 buttonSpider.addEventListener('click', () => {
+
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+
+});
+buttonRobin.addEventListener('click', () => {
+
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+
+});
+buttonBatman.addEventListener('click', () => {
 
     modal.classList.remove('hidden');
     modal.classList.add('visible');
